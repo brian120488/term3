040b01aac30c493eb950f7c3fcb66de09b8e1b3a
brian120488 <brian.yang@csedge.org>
2019-04-06 10:57:43 -0400

    Update day1/arrayJoin.js

--- day1/arrayJoin.js
+++ day1/arrayJoin.js
@@ -9,31 +9,32 @@ let phrases = ["enough", "zebras are great!", "are", "think they can", "crazy", 
  * in alphabetical order
 */
 
-
+phrases.sort();
 
 /* Remove the last element in the
  * phrases array
 */
 
-
+phrases.pop();
 
 /* Add the string "the ones who do."
  * to the end of the phrases array
 */
 
+phrases.push("the ones who do");
 
 
 /* Add the string "People who" at 
  * index 0 of the phrases array
 */
 
-
+phrases.splice(0,0,"People who");
 
 /* Add the string "to" at 
  * index 4 of the phrases array
 */
 
-
+phrases.splice(4,0,"to");
 
 /* Replace the string "umbrellas"
  * with the string at index 1 of the
@@ -42,14 +43,14 @@ let phrases = ["enough", "zebras are great!", "are", "think they can", "crazy", 
  * the current index of "umbrella".
 */
 
-
-
+//console.log(phrases.indexOf("umbrellas"));
+phrases[phrases.indexOf("umbrellas")] = phrases[1];
 
 /* Add the phrase "change the world," 
  * at the second to last index of the array.
 */
 
-
+phrases.splice(phrases.length - 2,0,"change the world");
 
 /* This code should print: 
  * People who are crazy enough to 
